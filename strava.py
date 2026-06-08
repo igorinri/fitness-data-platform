a06cd24a22729acacbf5dacaf8ec7ec2e344c1ad
Igor Nunes <igorinri@gmail.com>
2026-06-08 12:39:20 -0300

    Update strava.py
    Clear credencial

--- strava.py
+++ strava.py
@@ -3,7 +3,7 @@ import os
 
 
 # 🔹 Token do Strava (depois vamos mover para ENV)
-#STRAVA_TOKEN = "843db69feaedfb23be32a20c3eed0a7ac932e49b"
+#STRAVA_TOKEN = "8xpto"
 STRAVA_TOKEN = os.getenv("STRAVA_TOKEN")
 
 def fetch_activities(after=None):
